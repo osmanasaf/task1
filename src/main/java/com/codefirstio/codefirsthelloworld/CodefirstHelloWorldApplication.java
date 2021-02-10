36aec19643eb5e8d8990555772362a44d1d14b6d
Osman TATAR <osmaan.tatar@gmail.com>
2021-02-10 12:57:31 +0300

    ilk commit

---
+++ src/main/java/com/codefirstio/codefirsthelloworld/CodefirstHelloWorldApplication.java
@@ -0,0 +1,15 @@
+package com.codefirstio.codefirsthelloworld;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CodefirstHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodefirstHelloWorldApplication.class, args);
+	}
+
+}
